ea7c57d766ace019383ac0b026e357c6c374a20f
sautmanurung1 <smanurung360@gmail.com>
2021-09-09 17:25:58 +0700

    First commit

---
+++ src/config/router/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Home, Login, Register } from '../../pages'
+
+const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+export default Routes
+

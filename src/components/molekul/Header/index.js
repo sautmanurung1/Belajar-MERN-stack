0ff1c334b72bad670b4b959373bb31beaef62400
sautmanurung1 <smanurung360@gmail.com>
2021-09-12 16:31:06 +0700

    change in header

--- src/components/molekul/Header/index.js
+++ src/components/molekul/Header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
     const history = useHistory();
     return (
         <div className="header">
-            <p className="logo-app" onClick={() => history.push('/')}>Belajar MERN-Blog</p>
+            <p className="logo-app">Belajar MERN-Blog</p>
             <p className="menu-item" onClick={() => history.push('/login')}>Logout</p>
         </div>
     )
